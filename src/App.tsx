37e33333789a1491ee427ad559b4b580fbf0f850
Aaron Wilder <element1028@gmail.com>
2020-05-01 00:55:18 -0700

    updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { useRandomGif } from "./api";
+import { useTrendingGifs } from "./api";
 import "./App.css";
 
 function App() {
-  const { error, loading, data: gifRes } = useRandomGif({});
+  const { error, loading, data: gifRes } = useTrendingGifs({});
   return (
     <div className="App">
       <h1>Restful-React Giphy Test!</h1>
       {error && <h1>{error.message}</h1>}
       {loading && <h1>Loading....</h1>} :
-      {gifRes && (
-        <img alt="Giphy Giphy" src={gifRes.data?.images?.original?.url} />
-      )}
+      {gifRes &&
+        gifRes.data?.map((gif) => (
+          <img key={gif.id} alt={gif.slug} src={gif.images?.original?.url} />
+        ))}
     </div>
   );
 }
