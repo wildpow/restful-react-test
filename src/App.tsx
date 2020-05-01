0d865c5a94da857b103f5239a3ed0292cef1c840
Aaron Wilder <element1028@gmail.com>
2020-04-30 23:12:11 -0700

    testing out restful-react package
    - updated deps
    - typescript templete CRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useGet } from "restful-react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { data: randomDogImage } = useGet({
+    path: "breeds/image/random",
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <img
+          alt="Here's a good boye!"
+          src={randomDogImage && randomDogImage.message}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { RestfulProvider } from "restful-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RestfulProvider base="https://dog.ceo/api">
+      <App />
+    </RestfulProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
